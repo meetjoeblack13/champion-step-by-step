491e4afecd034954818faf10ebf7c343da7463d8
dmitrych <junkchrt@gmail.com>
2025-11-28 18:36:32 +0300

    Add functions parsePackage & DayActionInfo to package daysteps

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -2,6 +2,9 @@ package daysteps
 
 import (
 	"time"
+	"strings"
+	"fmt"
+	"strconv"
 )
 
 const (
@@ -12,9 +15,34 @@ const (
 )
 
 func parsePackage(data string) (int, time.Duration, error) {
-	// TODO: реализовать функцию
+	parts := strings.Split(data, ",")
+	if len(parts) != 2 {
+return 0, 0, fmt.Errorf("Ошибка ввода!")
+	}
+steps, err := strconv.Atoi(parts[0])
+if err != nil || steps < 0 {
+return 0, 0, fmt.Errorf("Ошибка подсчета количества шагов")
+}
+walkDuration, err := time.ParseDuration(parts[1])
+if err != nil || walkDuration < 0 {
+return 0, 0, fmt.Errorf("Ошибка подсчета продолжительности прогулки")
+}
+return steps, walkDuration, nil
 }
 
 func DayActionInfo(data string, weight, height float64) string {
-	// TODO: реализовать функцию
+	steps, walkDuration, err := parsePackage(data)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	_ = walkDuration // zaglushka
+	if steps == 0 {
+		return ""
+	}
+	distanceMeters := float64(steps) * stepLength
+distanceKilometers := distanceMeters / float64(mInKm)
+calories := 0.0 //zaglushka
+return fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.", 
+steps, distanceKilometers, calories)
 }
